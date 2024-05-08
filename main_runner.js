466365b5fad876ae0ca125000616f6ccfec06f1d
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2024-05-08 20:43:57 +1000

    bb

--- main_runner.js
+++ main_runner.js
@@ -1,19 +1,26 @@
-
+import Sprite from './sprite.js'
 
 export default class {
     #ctx
 
     constructor(canvas) {
-        const ctx = canvas.getContext("2d");
+        const ctx = canvas.getContext("2d")
         this.#ctx = ctx
     }
 
-    loadImages() {
+    run() {
+        //this.#loadImages()
+        const s = new Sprite(this.#ctx)
+        s.loadImages()
+        s.drawImage()
+    }
+
+    #loadImages() {
         const img = new Image(); // Create new img element
         img.addEventListener("load", () => {
-            this.#ctx.drawImage(img, 10, 10, 100, 100);
+            this.#ctx.drawImage(img, 10, 10, 100, 100)
         });
-        img.src = "test_img.png";
+        img.src = "test_img.png"
     }
 
 }
---
+++ sprite.js
@@ -0,0 +1,22 @@
+
+export default class {
+    #ctx
+    #imageLocations = ["test_img.png"]
+    #images = []
+
+    constructor(ctx) {
+        this.#ctx = ctx
+    }
+
+    loadImages() {
+        const img = new Image(); // Create new img element
+        img.src = "test_img.png"
+        await img.decode();
+        this.#images.push(img)
+    }
+
+    drawImage() {
+        this.#ctx.drawImage(this.#images[0])
+    }
+
+}
