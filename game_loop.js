6f353459bf66e220cedc40ea2eb0b273c827ca2b
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2024-05-11 07:17:41 +1000

    nn

--- game_loop.js
+++ game_loop.js
@@ -6,6 +6,19 @@ export default class {
 
     constructor(ctx) {
         this.#ctx = ctx
+        this.keyChecker()
+    }
+
+    keyChecker() {
+        const valueKeyMap = {
+            38:'↑',40:'↓',37:'←',39:'→',32:'spacebar',65:'a',66:'b',67:'c',68:'d',69:'e',70:'f',71:'g',72:'h',73:'i',74:'j',75:'k',76:'l',77:'m',78:'n',79:'o',80:'p',81:'q',82:'r',83:'s',84:'t',85:'u',86:'v',87:'w',88:'x',89:'y',90:'z',49:'1',50:'2',51:'3',52:'4',53:'5',54:'6',55:'7',56:'8',57:'9',48:'0'
+          };
+
+        document.addEventListener("keydown", (event) => {
+            if (event.locaton == 38) {
+                console.log('up')
+            }
+        })
     }
 
     async addEntity(entity) {
