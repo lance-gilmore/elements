d1303dc10ecc20392b2301ac1e3cc8c47ae68ded
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2024-05-12 15:24:05 +1000

    nn

--- sprites/mushroom.js
+++ sprites/mushroom.js
@@ -2,7 +2,7 @@ import Sprite from '../sprite_drawable.js'
 
 export default class extends Sprite {
     imageLocation = "bunny_sprites.png"
-    spriteLocation = [645,809,27,23]
+    spriteLocation = [646,810,26,22]
     
     constructor(ctx, x, y, w, h) {
         super(ctx, 300, 380, 52, 45)
