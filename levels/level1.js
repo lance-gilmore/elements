908158e3981b1324c473b57ef9f4cd7425d62592
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2024-05-19 07:10:37 +1000

    nn

--- levels/level1.js
+++ levels/level1.js
@@ -68,7 +68,7 @@ export default class extends Level {
 
     update() {
         const centerPoint = (this.bunny.positionx + this.girl.positionx) / 2
-        //this.viewx = (this.viewWidth / 2) - centerPoint
+        this.viewx = (this.viewWidth / 2) + centerPoint
 
         this.bunny.update()
         this.girl.update()
