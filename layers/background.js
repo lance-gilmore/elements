e25e92cf502ba8e8f450ab840b356a66cef7b7e9
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2024-05-19 09:07:33 +1000

    nn

--- layers/background.js
+++ layers/background.js
@@ -8,17 +8,17 @@ export default class extends Drawable {
     }
 
     async load() {
-        const m = new Mountains(this.ctx, -800, 0, 800, 600)
-        await m.load()
-        this.elements.push(m)
+        const m1 = new Mountains(this.ctx, -800, 0, 800, 600)
+        await m1.load()
+        this.elements.push(m1)
 
         const m = new Mountains(this.ctx, 0, 0, 800, 600)
         await m.load()
         this.elements.push(m)
 
-        const m = new Mountains(this.ctx, 800, 0, 800, 600)
-        await m.load()
-        this.elements.push(m)
+        const m2 = new Mountains(this.ctx, 800, 0, 800, 600)
+        await m2.load()
+        this.elements.push(m2)
     }
 
     update() {
