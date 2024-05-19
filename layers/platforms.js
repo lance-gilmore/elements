856989c17091e4b27e62511628c9306aa114c20a
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2024-05-19 18:28:03 +1000

    nn

--- layers/platforms.js
+++ layers/platforms.js
@@ -10,12 +10,20 @@ export default class extends Drawable {
     async load() {
         const p = new Platform(this.ctx,100,420)
         await p.load()
+        this.elements.push(p)
 
         const p2 = new Platform(this.ctx,250,420)
         await p2.load()
-
-        this.elements.push(p)
         this.elements.push(p2)
+
+        const p3 = new Platform(this.ctx,500,420)
+        await p3.load()
+        this.elements.push(p3)
+        
+        const p4 = new Platform(this.ctx,-50,300)
+        await p4.load()
+        this.elements.push(p4)
+        
     }
 
     update() {
