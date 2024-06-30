fba56b28c209fd3782bd88ba43b0dc84a5a56b76
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2024-06-30 10:21:58 +1000

    nn

--- sprites/inside_house.js
+++ sprites/inside_house.js
@@ -2,7 +2,7 @@ import Sprite from '../engine/sprite_drawable.js'
 
 export default class extends Sprite {
     imageLocation = "images/inside_house.png"
-    spriteLocation = [0,0,500,500]
+    spriteLocation = [0,0,354,490]
     
     constructor(ctx, x, y, w, h) {
         super(ctx, x, y, w, h)
