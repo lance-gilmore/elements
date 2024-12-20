bab7c312c886517e1b92ea851ba30c339996c918
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2024-12-20 13:24:59 +1000

    nn

--- layers/test_layer.js
+++ layers/test_layer.js
@@ -8,11 +8,10 @@ export default class extends JsonLayer {
 
     async load() {
         const json = 
-        [{"w":700,"h":40,"x":3209.39990234375,"y":569.9999847412109,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":700,"h":40,"x":3909.39990234375,"y":567.9999847412109,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":700,"h":40,"x":4608.5999755859375,"y":567.9999847412109,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":700,"h":40,"x":5310.4000244140625,"y":568.9999847412109,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":700,"h":40,"x":6010.600036621094,"y":568.9999847412109,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":700,"h":40,"x":6707.200042724609,"y":567.9999847412109,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"}]        
-        
+        [{"w":700,"h":40,"x":641.3999633789062,"y":570.0000267028809,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":700,"h":40,"x":1341.399990081787,"y":568.0000267028809,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"}]        
 
 
-        
+
         super.load(json)
     }
 
