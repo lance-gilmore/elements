0c4032c3febebed65fec809bc6aa119323a4cadb
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2024-12-21 13:43:36 +1000

    nn

--- layers/layer_data.js
+++ layers/layer_data.js
@@ -1,8 +1,8 @@
 
 export default class {
-    background = [{"w":700,"h":40,"x":641.3999633789062,"y":570.0000267028809,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":700,"h":40,"x":1341.399990081787,"y":568.0000267028809,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"}]        
+    background = [{"w":100,"h":100,"x":680.6500120162964,"y":481.1750030517578,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":100,"h":100,"x":2.6374998092651367,"y":481.1750030517578,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":100,"h":100,"x":1.625,"y":2.162506103515625,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":100,"h":100,"x":679.6500120162964,"y":4.162506103515625,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"}]
 
 
-    layer1 = [{"w":700,"h":40,"x":641.3999633789062,"y":570.0000267028809,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":700,"h":40,"x":1341.399990081787,"y":568.0000267028809,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"}]        
+    layer1 = [{"w":100,"h":100,"x":357.6500120162964,"y":248.1750030517578,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":100,"h":100,"x":251.6500120162964,"y":249.1750030517578,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"}]
 
 }
