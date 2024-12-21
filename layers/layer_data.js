2fc33d7aab629e42174da05fb389ec0a0e87744c
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2024-12-21 10:53:01 +1000

    nn

--- layers/layer_data.js
+++ layers/layer_data.js
@@ -1,5 +1,8 @@
 
 export default class {
+    background = [{"w":700,"h":40,"x":641.3999633789062,"y":570.0000267028809,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":700,"h":40,"x":1341.399990081787,"y":568.0000267028809,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"}]        
+
+
     layer1 = [{"w":700,"h":40,"x":641.3999633789062,"y":570.0000267028809,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":700,"h":40,"x":1341.399990081787,"y":568.0000267028809,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"}]        
 
 }
