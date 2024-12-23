edbb5370949dba6357c9044d145af1f8a26465b3
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2024-12-23 11:26:39 +1000

    nn

--- layers/test_layer.js
+++ layers/test_layer.js
@@ -1,4 +1,5 @@
 import JsonLayer from '../engine/json_layer.js'
+import LayerData from '../layers/layer_data.js'
 
 export default class extends JsonLayer {
 
@@ -7,12 +8,9 @@ export default class extends JsonLayer {
     }
 
     async load() {
-        const json = 
-        [{"w":700,"h":40,"x":641.3999633789062,"y":570.0000267028809,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"},{"w":700,"h":40,"x":1341.399990081787,"y":568.0000267028809,"img":"https://cdnb.artstation.com/p/assets/images/images/010/604/427/large/nick-rossi-gam322-nrossi-m11-lava.jpg"}]        
+        const ld = new LayerData()
 
-
-
-        super.load(json)
+        super.load(ld.background)
     }
 
     update() {
