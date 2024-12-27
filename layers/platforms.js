c3266531d315bb07dbd3b7890c1a15dc46fc86f5
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2024-12-27 16:10:03 +1000

    nn

--- layers/platforms.js
+++ layers/platforms.js
@@ -1,30 +1,16 @@
-import Drawable from '../engine/layer.js'
-import Platform from '../sprites/platform.js'
+import JsonLayer from '../engine/json_layer.js'
+import LayerData from '../layers/layer_data.js'
 
-export default class extends Drawable {
+export default class extends JsonLayer {
 
     constructor(ctx, x, y, w, h) {
         super(ctx, x, y, w, h)
     }
 
     async load() {
-        const platformLocations =[
-            [100,420],
-            [250,420],
-            [500,420],
-            [-50,300],
-            [-400,0],
-            [-400,150],
-            [-400,300],
-            [-400,450]
-        ]
+        const ld = new LayerData()
 
-        for (const location of platformLocations) {
-            const p = new Platform(this.ctx,location[0],location[1])
-            await p.load()
-            this.elements.push(p)
-        }
-
+        super.load(ld.platforms)
     }
 
     update() {
@@ -32,3 +18,38 @@ export default class extends Drawable {
     }
 
 }
+
+// import Drawable from '../engine/layer.js'
+// import Platform from '../sprites/platform.js'
+
+// export default class extends Drawable {
+
+//     constructor(ctx, x, y, w, h) {
+//         super(ctx, x, y, w, h)
+//     }
+
+//     async load() {
+//         const platformLocations =[
+//             [100,420],
+//             [250,420],
+//             [500,420],
+//             [-50,300],
+//             [-400,0],
+//             [-400,150],
+//             [-400,300],
+//             [-400,450]
+//         ]
+
+//         for (const location of platformLocations) {
+//             const p = new Platform(this.ctx,location[0],location[1])
+//             await p.load()
+//             this.elements.push(p)
+//         }
+
+//     }
+
+//     update() {
+        
+//     }
+
+// }
