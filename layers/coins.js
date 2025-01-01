e4263364b561fd397a390cc00285d9264e8c6077
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2025-01-01 13:14:11 +1000

    nn

--- layers/coins.js
+++ layers/coins.js
@@ -1,5 +1,8 @@
 import JsonLayer from '../engine/json_layer.js'
 import LayerData from '../layers/layer_data.js'
+import AnimatedSprite from '../engine/animated_sprite.js'
+import ImageDrawable from '../engine/image_drawable.js'
+
 
 export default class extends JsonLayer {
 
@@ -9,8 +12,28 @@ export default class extends JsonLayer {
 
     async load() {
         const ld = new LayerData()
-
         super.load(ld.coins)
+
+        let as = new AnimatedSprite(this.ctx,100,100,40,40)
+
+        const layerImage = new ImageDrawable(this.ctx,100,100,40,40)
+        layerImage.imageLocation = "https://lance-gilmore.github.io/elements/images/coin.png"
+        await layerImage.load()
+        as.images.push(layerImage)
+
+        const layerImage2 = new ImageDrawable(this.ctx,100,100,40,40)
+        layerImage2.imageLocation = "https://lance-gilmore.github.io/elements/images/coin2.png"
+        await layerImage2.load()
+        as.images.push(layerImage2)
+
+        const layerImage3 = new ImageDrawable(this.ctx,100,100,40,40)
+        layerImage3.imageLocation = "https://lance-gilmore.github.io/elements/images/coin3.png"
+        await layerImage3.load()
+        as.images.push(layerImage3)
+
+
+
+        this.elements.push(as)
     }
 
     update() {
