87596658083c44fd906b132f915d8ce6eff6f2a2
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2025-01-02 10:31:08 +1000

    nn

--- layers/blue_monster.js
+++ layers/blue_monster.js
@@ -26,7 +26,7 @@ export default class extends JsonLayer {
 
     update() {
         const movementSpeed = 5
-        for (const element in this.elements) {
+        for (const element of this.elements) {
             console.log(element)
             if (this.#movingLeft) {
                 element.move(element.canvasx-movementSpeed, element.canvasy)
