5293563baa79c412186add017addda4f351f8fd1
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2025-01-03 09:00:57 +1000

    nn

--- layers/scores.js
+++ layers/scores.js
@@ -21,13 +21,13 @@ export default class extends Drawable {
         this.elements.push(layerImage1)
 
         const img2 = "https://lance-gilmore.github.io/elements/images/red_sphere.png"
-        const layerImage2 = new ImageDrawable(this.ctx,130+this.#offset,10,20,20)
+        const layerImage2 = new ImageDrawable(this.ctx,140+this.#offset,10,20,20)
         layerImage2.imageLocation = img2
         await layerImage2.load()
         this.elements.push(layerImage2)
 
         const img3 = "https://lance-gilmore.github.io/elements/images/blue_sphere.png"
-        const layerImage3 = new ImageDrawable(this.ctx,170+this.#offset,10,10,10)
+        const layerImage3 = new ImageDrawable(this.ctx,180+this.#offset,13,10,10)
         layerImage3.imageLocation = img3
         await layerImage3.load()
         this.elements.push(layerImage3)
@@ -49,11 +49,11 @@ export default class extends Drawable {
         super.draw()
 
         this.ctx.font = "20px Arial";
-        this.ctx.fillText(this.neutrons,120+this.#offset,28);
+        this.ctx.fillText(this.neutrons,115+this.#offset,28);
 
-        this.ctx.fillText(this.protons,150+this.#offset,28);
+        this.ctx.fillText(this.protons,160+this.#offset,28);
 
-        this.ctx.fillText(this.electrons,200+this.#offset,28);
+        this.ctx.fillText(this.electrons,210+this.#offset,28);
     }
 
     update() {
