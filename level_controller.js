368f0fb60f76ffe3d1dd182b3f644e7a2ef9a8d5
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2025-01-04 15:26:51 +1000

    nn

--- level_controller.js
+++ level_controller.js
@@ -1,40 +1,43 @@
 import Drawable from './engine/drawable.js'
-import Level1 from './levels/level1.js'
-import LevelHouse from './levels/level_house.js'
 import Store from './levels/store.js'
+import World from './levels/world.js'
+import Level1 from './levels/level1.js'
 
 export default class extends Drawable {
     
     controlls
-    #ctx
     currentLevel
 
     constructor(ctx, x, y, w, h, controlls) {
         super(ctx, x, y, w, h)
         this.controlls = controlls
-        this.#ctx = ctx
     }
 
     async load() {
-        
-        const l = new Level1(this.#ctx, 0, 0, this.canvasw, this.canvash, this.controlls)
+        this.loadWorld()
+    }
+
+
+    loadWorld() {
+        const l = new World(this.ctx, 0, 0, this.canvasw, this.canvash, this.controlls)
+        await l.load()
+        this.currentLevel = l
+        l.addExitLevelListener(() => {
+            this.loadLevel1();
+          })
+    }
+
+    loadLevel1() {
+        const l = new Level1(this.ctx, 0, 0, this.canvasw, this.canvash, this.controlls)
         await l.load()
         this.currentLevel = l
         l.addExitLevelListener(() => {
             this.loadStore();
           })
-
-    }
-
-    loadLevel2() {
-       const lh = new LevelHouse(this.#ctx, 0, 0, this.canvasw, this.canvash, this.controlls)
-       lh.load()
-
-       this.currentLevel = lh
     }
 
     loadStore() {
-       const lh = new Store(this.#ctx, 0, 0, this.canvasw, this.canvash, this.controlls)
+       const lh = new Store(this.ctx, 0, 0, this.canvasw, this.canvash, this.controlls)
        lh.load()
        this.currentLevel = lh
     }
