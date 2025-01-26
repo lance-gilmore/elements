14a1655cc85e028176e9dc9778004cc5faca1b17
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2025-01-26 16:06:07 +1000

    nn

---
+++ layer_data/elements.js
@@ -0,0 +1,116 @@
+export default class {
+    elements = [
+        {
+            'name': 'Hydrogen',
+            'symbol': 'H',
+            'protons': 1,
+            'neutrons': 0,
+            'electrons': 1
+        },
+        {
+            'name': 'Helium',
+            'symbol': 'He',
+            'protons': 2,
+            'neutrons': 2,
+            'electrons': 2
+        },
+        {
+            'name': 'Lithium',
+            'symbol': 'Li',
+            'protons': 3,
+            'neutrons': 4,
+            'electrons': 3
+        },
+        {
+            'name': 'Beryllium',
+            'symbol': 'Be',
+            'protons': 4,
+            'neutrons': 5,
+            'electrons': 4
+        },
+        {
+            'name': 'Boron',
+            'symbol': 'B',
+            'protons': 5,
+            'neutrons': 6,
+            'electrons': 5
+        },
+        {
+            'name': 'Carbon',
+            'symbol': 'C',
+            'protons': 6,
+            'neutrons': 6,
+            'electrons': 6
+        },
+        {
+            'name': 'Nitrogen',
+            'symbol': 'N',
+            'protons': 7,
+            'neutrons': 7,
+            'electrons': 7
+        },
+        {
+            'name': 'Oxygen',
+            'symbol': 'O',
+            'protons': 8,
+            'neutrons': 8,
+            'electrons': 8
+        },
+        {
+            'name': 'Fluorine',
+            'symbol': 'F',
+            'protons': 9,
+            'neutrons': 10,
+            'electrons': 9
+        },
+        {
+            'name': 'Neon',
+            'symbol': 'Ne',
+            'protons': 10,
+            'neutrons': 10,
+            'electrons': 10
+        },
+        {
+            'name': 'Sodium',
+            'symbol': 'Na',
+            'protons': 11,
+            'neutrons': 12,
+            'electrons': 11
+        },
+        {
+            'name': 'Magnesium',
+            'symbol': 'Mg',
+            'protons': 12,
+            'neutrons': 12,
+            'electrons': 12
+        },
+        {
+            'name': 'Aluminium',
+            'symbol': 'Al',
+            'protons': 13,
+            'neutrons': 13,
+            'electrons': 13
+        },
+        {
+            'name': 'Silicon',
+            'symbol': 'Si',
+            'protons': 14,
+            'neutrons': 14,
+            'electrons': 14
+        },
+        {
+            'name': 'Phosphorus',
+            'symbol': 'P',
+            'protons': 15,
+            'neutrons': 16,
+            'electrons': 15
+        },
+        {
+            'name': 'Sulfur',
+            'symbol': 'S',
+            'protons': 16,
+            'neutrons': 16,
+            'electrons': 16
+        },
+    ]
+}
---
+++ layers/store_items.js
@@ -0,0 +1,39 @@
+import Layer from '../engine/json_layer.js'
+import StoreItem from '../layers/store_item.js'
+
+export default class extends Layer {
+
+    constructor(ctx, x, y, w, h) {
+        super(ctx, x, y, w, h)
+    }
+
+    async load(json) {
+
+        for (const image of json) {
+            const layerImage = new StoreItem(this.ctx,image.x,image.y,image.w,image.h)
+            layerImage.imageLocation = image.img
+            await layerImage.load()
+            this.elements.push(layerImage)
+        }
+        
+    }
+
+    checkCollision(x,y,r,b) {
+        this.level = ''
+        for (const e of this.elements) {
+            const element = e.elements[0]
+            const ex = element.startx
+            const ey = element.starty
+            const er = element.startx + element.canvasw
+            const eb = element.starty + element.canvash
+            
+            if (x < er && r > ex && y < eb && b > ey) {
+                this.level = e.level
+                return true
+            }
+
+        }
+        return false
+    }
+
+}
