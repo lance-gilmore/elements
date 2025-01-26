2003ce8ee69adeb2a2ca98879e265a90254911c9
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2025-01-26 11:16:55 +1000

    nn

--- layers/world_door.js
+++ layers/world_door.js
@@ -49,31 +49,32 @@ export default class extends Drawable {
         await layerImage1.load()
         this.elements.push(layerImage1)
 
-        const img2 = "https://lance-gilmore.github.io/elements/images/proton1.png"
-        const layerImage2 = new ImageDrawable(this.ctx,80+this.#positionx,infoOffsetTop+this.#positiony,20,20)
-        layerImage2.imageLocation = img2
-        await layerImage2.load()
-        this.elements.push(layerImage2)
-
         const img3 = "https://lance-gilmore.github.io/elements/images/electron1.png"
-        const layerImage3 = new ImageDrawable(this.ctx,145+this.#positionx,5+infoOffsetTop+this.#positiony,10,10)
+        const layerImage3 = new ImageDrawable(this.ctx,80+this.#positionx,-5+infoOffsetTop+this.#positiony,10,10)
         layerImage3.imageLocation = img3
         await layerImage3.load()
         this.elements.push(layerImage3)
 
-        const textOffsetTop = 65
+        const img2 = "https://lance-gilmore.github.io/elements/images/proton1.png"
+        const layerImage2 = new ImageDrawable(this.ctx,140+this.#positionx,infoOffsetTop+this.#positiony,20,20)
+        layerImage2.imageLocation = img2
+        await layerImage2.load()
+        this.elements.push(layerImage2)
+
+
+        const textOffsetTop = 63
         const fontSize = "16"
 
         const txt1 = new TextDrawable(this.ctx,10+this.#positionx,textOffsetTop+this.#positiony,fontSize)
         txt1.text = this.scores.neutrons + "/" + this.neutrons
         this.elements.push(txt1)
 
-        const txt2 = new TextDrawable(this.ctx,75+this.#positionx,textOffsetTop+this.#positiony,fontSize)
-        txt2.text = this.scores.protons + "/" + this.protons
+        const txt2 = new TextDrawable(this.ctx,75+this.#positionx,-5+textOffsetTop+this.#positiony,fontSize)
+        txt2.text = this.scores.electrons + "/" + this.electrons
         this.elements.push(txt2)
 
         const txt3 = new TextDrawable(this.ctx,135+this.#positionx,textOffsetTop+this.#positiony,fontSize)
-        txt3.text = this.scores.electrons + "/" + this.electrons
+        txt3.text = this.scores.protons + "/" + this.protons
         this.elements.push(txt3)
     }
 
